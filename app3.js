3a19cea9bb57ac0fdb9cb1e9867ecda9550b6286
Rishabh Bhandari <41220684+RishabhKodes@users.noreply.github.com>
2020-04-18 16:08:40 +0530

    Update app3.js

--- app3.js
+++ app3.js
@@ -1,13 +1,16 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-      res.end('Hello World');
+app.post('/', function (req, res) {
+      res.end({"message":"Hello World"});
 })
 
 var server = app.listen(process.env.PORT, function () {
-//    var host = server.address().address
+      if (err){
+            console.log(err);
+      }
+      else{
    var port = server.address().port
    console.log("Example app listening at port %s", port)
-})
+            }})
 
